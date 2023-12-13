f1f7d6ad2c86a05d0495ef9c4c7fe5d5331bd5f6
renegirresse <renegirresse@gmail.com>
2023-12-13 11:39:55 +0000

    #50

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-alert('bem-vindo ao jogo do número secreto');
+alert('bem-vindo ao jogo do número secreto');git 
 let numeroSecreto = 50
 let chute = prompt('qual o seu palpite para o número secreto?')
